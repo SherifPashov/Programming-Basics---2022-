794389adc7af6e343f6e93c8e10ca8f96c2d6db9
SherifPashov <152787933+SherifPashov@users.noreply.github.com>
2023-12-17 19:01:19 +0200

    Update 04. Train The Trainers original.cs
    add /n

--- 06.Nested Loops - Exercise/04. Train The Trainers original.cs
+++ 06.Nested Loops - Exercise/04. Train The Trainers original.cs
@@ -8,11 +8,14 @@ namespace _04._Train_The_Trainers_original
         {
             int jury = int.Parse(Console.ReadLine());
             string tema = Console.ReadLine();
+            
             double numberTema = 0;
             double sumOcenki = 0;
+            
             while (tema != "Finish")
             {
                 double averageTema = 0;
+                
                 for (int i = 0; i < jury; i++)
                 {
                     double ocenka = double.Parse(Console.ReadLine());
@@ -20,8 +23,11 @@ namespace _04._Train_The_Trainers_original
                     sumOcenki += ocenka;
                     numberTema++;
                 }
+                
                 averageTema /= jury;
+                
                 Console.WriteLine($"{tema} - {averageTema:f2}.");
+                
                 tema = Console.ReadLine();
             }
             
